26edd4ea46cf2a4c32ba47c4d500a42cb171a47d
zhaogm <865301453@qq.com>
2026-05-31 23:36:14 +0800

    refactor: replace neo-tree with nvim-tree (Ctrl+N toggle)

---
+++ lua/plugins/nvim-tree.lua
@@ -0,0 +1,49 @@
+return {
+    {
+        "nvim-tree/nvim-tree.lua",
+        version = "*",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        keys = {
+            { "<C-n>", function()
+                local api = require("nvim-tree.api")
+                if not api.tree.is_visible() then
+                    api.tree.open()
+                elseif api.tree.is_tree_buf() then
+                    api.tree.close()
+                else
+                    api.tree.focus()
+                end
+            end, desc = "切换目录树" },
+        },
+        config = function()
+            require("nvim-tree").setup({
+                view = {
+                    width = 30,
+                },
+                renderer = {
+                    icons = {
+                        show = { git = true },
+                    },
+                },
+                on_attach = function(bufnr)
+                    local api = require("nvim-tree.api")
+                    local function opts(desc)
+                        return { desc = "nvim-tree: " .. desc, buffer = bufnr, nowait = true }
+                    end
+                    -- l 打开文件（不关树）
+                    vim.keymap.set("n", "l", api.node.open.edit, opts("Open"))
+                    -- Enter 打开文件并关闭树
+                    vim.keymap.set("n", "<CR>", function()
+                        api.node.open.edit()
+                        api.tree.close()
+                    end, opts("Open & Close"))
+                end,
+                actions = {
+                    open_file = {
+                        quit_on_open = false,
+                    },
+                },
+            })
+        end,
+    },
+}
