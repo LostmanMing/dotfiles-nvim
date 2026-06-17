24e93198c4d988cc1b8e1d59e5c95a9d0aff423b
zhaogm <zhaogm@U-TJM7T56Q-1816.local>
2026-06-17 11:42:37 +0800

    feat: add terminal-mode keymaps to create and switch terminals
    Ctrl+n creates a new float terminal, Ctrl+] / Ctrl+[ cycles
    between existing terminals without leaving terminal mode.

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -9,6 +9,39 @@ return {
             { "<leader>th", "<cmd>ToggleTerm direction=horizontal<CR>", desc = "水平终端" },
             { "<leader>tv", "<cmd>ToggleTerm direction=vertical<CR>", desc = "垂直终端" },
         },
+        config = function(_, opts)
+            require("toggleterm").setup(opts)
+            vim.keymap.set("t", "<C-]>", function()
+                local terms = require("toggleterm.terminal").get_all()
+                if #terms < 2 then return end
+                local cur = require("toggleterm.terminal").get(tonumber(vim.b.toggle_number))
+                local idx
+                for i, t in ipairs(terms) do
+                    if t.id == cur.id then idx = i break end
+                end
+                local next_term = terms[idx % #terms + 1]
+                cur:close()
+                next_term:open()
+            end, { desc = "下一个终端" })
+            vim.keymap.set("t", "<C-[>", function()
+                local terms = require("toggleterm.terminal").get_all()
+                if #terms < 2 then return end
+                local cur = require("toggleterm.terminal").get(tonumber(vim.b.toggle_number))
+                local idx
+                for i, t in ipairs(terms) do
+                    if t.id == cur.id then idx = i break end
+                end
+                local prev_term = terms[(idx - 2) % #terms + 1]
+                cur:close()
+                prev_term:open()
+            end, { desc = "上一个终端" })
+            vim.keymap.set("t", "<C-n>", function()
+                local terms = require("toggleterm.terminal").get_all()
+                local new_id = #terms + 1
+                vim.cmd(new_id .. "ToggleTerm direction=float")
+                vim.schedule(function() vim.cmd("startinsert") end)
+            end, { desc = "新建终端" })
+        end,
         opts = {
             size = 20,
             open_mapping = [[<c-\>]],
