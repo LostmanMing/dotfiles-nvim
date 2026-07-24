c838773e955ff9831771f3ec25f1c85be42fa849
zhaogm <865301453@qq.com>
2026-07-24 07:08:34 +0000

    feat(flash): allow flash jump in terminal buffers (normal mode)

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -5,11 +5,12 @@ return {
         opts = {},
         keys = {
             { "<leader>s", mode = { "n", "x", "o" }, function()
-                if vim.bo.buftype ~= "" then return end  -- Telescope 等特殊 buffer 里不激活
+                -- 允许普通文件和终端（终端普通模式）里用；其它特殊 buffer（Telescope 等）不激活
+                if vim.bo.buftype ~= "" and vim.bo.buftype ~= "terminal" then return end
                 require("flash").jump()
             end, desc = "Flash 字符跳转" },
             { "<leader>S", mode = { "n", "x", "o" }, function()
-                if vim.bo.buftype ~= "" then return end
+                if vim.bo.buftype ~= "" and vim.bo.buftype ~= "terminal" then return end
                 require("flash").treesitter()
             end, desc = "Flash 选中语法节点" },
         },
